92469327e4bb6c52f08883cf4a7408fef6f1f619
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2017-08-23 18:48:26 +0300

    bootstr

--- stoolandchair/index.php
+++ stoolandchair/index.php
@@ -19,19 +19,96 @@
 <body>
 
     <div class="container">
-        <h1>Лучшие табуретки и стулья из фанеры</h1>
+        
+        
+<br />
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Табуретка и стул</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                  </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
+            </nav>
 
-        <p>Изготавливаем из лучших сортов фанеры на ультрасовременном роботизированном оборудовании</p>
+<br />
+        <!-- Джумботрон -->
+                <div class="jumbotron">
+          <h1 class="display-3">Лучшие табуретки и стулья из фанеры!</h1>
+          <p class="lead">Изготавливаем из лучших сортов фанеры на ультрасовременном роботизированном оборудовании.</p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
+        
+        <!-- Карусель -->
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        
+              <div class="carousel-inner">
+                  
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="img/chair1.jpg" alt="First slide">
+                </div>
+                  
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/stool1.jpg" alt="Second slide">
+                </div>
+                  
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/chair2.jpg" alt="Third slide">
+                </div>
+                  
+              </div>
+                        
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+        
+        <br />
+                <ul class="list-group">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+  <li class="list-group-item">Porta ac consectetur ac</li>
+  <li class="list-group-item">Vestibulum at eros</li>
+</ul>
+        
+        <br />
+     
+      
         <?php
 
 
         $i=glob('img/*');
         foreach($i as $v) {
         ?>
-
-        <img src="<?=$v?>" width="350" />
-
+        <figure class="figure">
+            <img class="figure-img img-fluid rounded" src="<?=$v?>" />
+        </figure>
         <?php
         }
 
