27b574a0c0df0430c642dca0e3734740854ab8d2
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2017-08-24 14:40:44 +0300

    bstr

--- stoolandchair/index.php
+++ stoolandchair/index.php
@@ -52,53 +52,96 @@
                 <div class="jumbotron">
           <h1 class="display-3">Лучшие табуретки и стулья из фанеры!</h1>
           <p class="lead">Изготавливаем из лучших сортов фанеры на ультрасовременном роботизированном оборудовании.</p>
+                    <!--
           <hr class="my-4">
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        -->
+                    <!--
           <p class="lead">
             <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
           </p>
+                -->
         </div>
         
-        <!-- Карусель -->
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        
-              <div class="carousel-inner">
-                  
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src="img/chair1.jpg" alt="First slide">
-                </div>
-                  
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="img/stool1.jpg" alt="Second slide">
-                </div>
-                  
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="img/chair2.jpg" alt="Third slide">
-                </div>
-                  
-              </div>
-                        
-              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
+        <p>Вся наша мебель поставляется в плоской упаковке.</p>
         
-        <br />
-                <ul class="list-group">
-  <li class="list-group-item">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
-</ul>
+        <!-- 
+        КОНТЕНТ
+        -->
         
-        <br />
-     
+         <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="rounded-circle" src="img/stool2.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Табуреты</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="rounded-circle" src="img/chair2.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Стулья</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="rounded-circle" src="img/chair3.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Кресла</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Разборный стул №1. <span class="text-muted">It'll blow your mind.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" src="img/chair2.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <img class="featurette-image img-fluid mx-auto" src="img/chair3.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-fluid mx-auto" src="img/chair5.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+        
+        
       
         <?php
 
@@ -106,9 +149,8 @@
         $i=glob('img/*');
         foreach($i as $v) {
         ?>
-        <figure class="figure">
-            <img class="figure-img img-fluid rounded" src="<?=$v?>" />
-        </figure>
+            <img class="rounded" width="100%" src="<?=$v?>" />
+        <br />
         <?php
         }
 
