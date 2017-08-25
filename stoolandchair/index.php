725af9eeeffbb25a5de04c6435393108078f833b
John Green <gohngreen>
2017-08-25 20:55:37 +0300

    123

--- stoolandchair/index.php
+++ stoolandchair/index.php
@@ -125,8 +125,8 @@
 
       <div class="row featurette">
         <div class="col-md-7 order-md-2">
-          <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          <h2 class="featurette-heading">Разборный стул "Guido"<span class="text-muted">ХИТ ПРОДАЖ.</span></h2>
+          <p class="lead">Габариты(мм) ДхШхВ: 518х636х846. Материал: березавая фанера. Покрытие: полиуретан/краска/лак/б.покрытия</p>
         </div>
         <div class="col-md-5 order-md-1">
           <img class="featurette-image img-fluid mx-auto" src="img/chair16.jpg" alt="Generic placeholder image">
