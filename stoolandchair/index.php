b074a88e9f6b273e7b8267aac073b29b51909903
egor <egor@macs-MacBook.local>
2017-08-25 23:44:06 +0300

    impr

--- stoolandchair/index.php
+++ stoolandchair/index.php
@@ -69,57 +69,9 @@
         КОНТЕНТ
         -->
         
-         <!-- Marketing messaging and featurettes
-    ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
-
-    <!-- <div class="container marketing"> -->
-
-       <!-- <!-- Three columns of text below the carousel -->	    
-      <!-- <div class="row"> -->
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/stool2.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Табуреты</h2> -->
-          <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair2.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Стулья</h2> -->
-          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-	    <!-- </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Стулья</h2> -->
-          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-	    <!-- </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Стулья</h2> -->
-          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Стулья</h2> -->
-          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Стулья</h2> -->
-          <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> 
-        <!-- <div class="col-lg-4"> -->
-          <!-- <img class="rounded-circle" src="img/chair3.jpg" alt="Generic placeholder image" width="140" height="140"> -->
-          <!-- <h2>Кресла</h2> -->
-          <!-- <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> 
-      <!-- </div><!-- /.row -->  
 
       <!-- START THE FEATURETTES -->
+        
 <!--rigth text Alignment-->
       <hr class="featurette-divider">
 
@@ -128,7 +80,7 @@
           <h2 class="featurette-heading">Стул &laquo;Гвидо&raquo;
               <span class="badge badge-primary">Хит продаж!</span></h2>
             
-          <p class="lead">Габариты: 518&times;636&times;846 см.
+          <p class="lead">Габариты: 51&times;63&times;84 см.
               <br>Материал: 
               <span class="badge badge-light">березовая фанера</span>
               <br>Покрытие:
