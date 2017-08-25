e54e77e2002231258651acffff8426eb13f7138c
John Green <gohngreen>
2017-08-25 19:58:48 +0300

    del other photo

--- stoolandchair/index.php
+++ stoolandchair/index.php
@@ -75,49 +75,49 @@
 
     <!-- <div class="container marketing"> -->
 
-       <!-- <!-- Three columns of text below the carousel -->	    -->
+       <!-- <!-- Three columns of text below the carousel -->	    
       <!-- <div class="row"> -->
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/stool2.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Табуреты</h2> -->
           <!-- <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p> -->
           <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> -->
+        <!-- </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair2.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Стулья</h2> -->
           <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
           <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-	    <!-- </div><!-- /.col-lg-4 --> -->
+	    <!-- </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Стулья</h2> -->
           <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
           <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-	    <!-- </div><!-- /.col-lg-4 --> -->
+	    <!-- </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Стулья</h2> -->
           <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> -->
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Стулья</h2> -->
           <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
-          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> -->
+          <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>	    </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair5.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Стулья</h2> -->
           <!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p> -->
           <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> -->
+        <!-- </div><!-- /.col-lg-4 --> 
         <!-- <div class="col-lg-4"> -->
           <!-- <img class="rounded-circle" src="img/chair3.jpg" alt="Generic placeholder image" width="140" height="140"> -->
           <!-- <h2>Кресла</h2> -->
           <!-- <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p> -->
           <!-- <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p> -->
-        <!-- </div><!-- /.col-lg-4 --> -->
-      <!-- </div><!-- /.row -->  -->
+        <!-- </div><!-- /.col-lg-4 --> 
+      <!-- </div><!-- /.row -->  
 
       <!-- START THE FEATURETTES -->
 <!--rigth text Alignment-->
@@ -401,19 +401,19 @@
         
         
       
-        <?php
+        <!-- <?php -->
 
 
-        $i=glob('img/*');
-        foreach($i as $v) {
-        ?>
-            <img class="rounded" width="100%" src="<?=$v?>" />
-        <br />
-        <?php
-        }
+        <!-- $i=glob('img/*'); -->
+        <!-- foreach($i as $v) { -->
+        <!-- ?> -->
+            <!-- <img class="rounded" width="100%" src="<?=$v?>" /> -->
+        <!-- <br /> -->
+        <!-- <?php -->
+        <!-- } -->
 
 
-        ?>
+        <!-- ?> -->
     
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
